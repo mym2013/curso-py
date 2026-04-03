36970e7b313092e48f12501575e19bd81ceb932a
Gonzalo <torresdelfierro@gmail.com>
2026-04-03 13:59:41 -0300

    feat: complete basic types (string, numbers, math) y haremos una calculadora ahora

---
+++ 02-tipos-basicos/numeros-funciones.py
@@ -0,0 +1,18 @@
+import math
+
+print(round(1.6))
+print(round(1.5))
+print(round(4.2))
+print (abs(-77))
+print(abs(55))
+
+print(math.ceil(1.1))
+print(math.floor(1.9999))
+print(math.isnan(23))
+#print(math.isnan("2.3"))
+print(math.isnan(float("nan")))
+print(math.floor(math.pow(10,3)))
+
+print(math.floor(math.sqrt(9)))
+
+
---
+++ 02-tipos-basicos/numeros.py
@@ -0,0 +1,17 @@
+num = 2 # entero ---> integer
+decimal = 1.2 #float
+imagi = 2 + 2j # num imaginario
+
+print(1+3)
+print(1-3)
+print(1*3)
+
+print(1/3)
+
+print(1//3)
+
+print(8/3)
+
+print(2**3) 
+num += 2 
+print(num)
