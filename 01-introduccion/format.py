44f2a9909d779aaf78ea9fc08e54cc44f788104b
Gonzalo <torresdelfierro@gmail.com>
2026-04-02 22:09:35 -0300

    feat(cap01): introduccion y primeros scripts

---
+++ 01-introduccion/format.py
@@ -0,0 +1,6 @@
+emoción= "Feliz"
+a = 12 
+b = 45
+print(emoción)
+print(a)
+print(b)
---
+++ 01-introduccion/intro.py
@@ -0,0 +1,7 @@
+print("primer print de @gtdel hacer rato")
+print ( "vamos!!"*7)
+nombre = "Gonzalo"
+edad = 30
+
+print(nombre)
+print(edad)
