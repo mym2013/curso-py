2af200e88bde450af6cf34d438f43014ac42ee3a
Gonzalo <torresdelfierro@gmail.com>
2026-04-05 10:19:08 -0400

    Comenzando el cap4

--- 04-funciones/funciones.py
+++ 04-funciones/funciones.py
@@ -1,7 +1,19 @@
 
 
-def sldo():
-    print("hello")
-    print("que pasa")
+# def sldo(nombre,apellido):
+#     print("hello")
+#     print(f"que pasa {nombre} {apellido}")
 
-sldo()
+# sldo("Gonzalin", "bombin")
+# sldo("angelica", "mama")
+
+#parametros opcionales 
+def sldo(nombre,apellido="happy human"): # apellido="happy human, a esto se le llaman, parámtetos opcionales
+    print("hello")
+    print(f"que pasa {nombre} {apellido}")
+
+sldo("Gonzalin", "bombin")
+sldo("angelica")
+
+sldo("Fernando") 
+sldo( apellido ="Torres", nombre = "Gonzalo")
